7eb11cc52a2860e3bb3bc86664e01255df9ea747
ADernild <alex@dernild.dk>
2022-01-24 11:09:26 +0100

    plotting class distributions

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -7,6 +7,8 @@ Data preparation
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 os.chdir("../")
 
 #%% Importing data and encoding new colums
@@ -15,6 +17,13 @@ df = pd.read_csv("data/DIDA_12000_String_Digit_Labels.csv", header=None)
 
 df['path'] = "DIDA_1/" + df[0].astype(str) + ".jpg"
 df['CC'] = np.select([(df[1]<1900) & (df[1]>1799)], [0], default=1)
-df['D'] = np.select([(df[1].astype(str).str.len()>1) & (df[1].astype(str).str.len()<5)], [df[1].astype(str).str[-2]], default=10)
-df['Y'] = np.select([(df[1].astype(str).str.len()>1) & (df[1].astype(str).str.len()<5)], [df[1].astype(str).str[-1]], default=10)
+df['D'] = np.select([(df[1].astype(str).str.len()>1) & (df[1].astype(str).str.len()<5)], [df[1].astype(str).str[-2].astype(int)], default=10)
+df['Y'] = np.select([(df[1].astype(str).str.len()>1) & (df[1].astype(str).str.len()<5)], [df[1].astype(str).str[-1].astype(int)], default=10)
 
+#%% Plotting class distributions
+
+sns.histplot(df['CC'])
+plt.figure()
+sns.histplot(df['D'])
+plt.figure()
+sns.histplot(df['Y'])
