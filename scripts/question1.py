c84cb990fac1a71de7d0a270bb6278e0ef5f0863
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2022-01-27 13:25:09 +0100

    Added spaces

--- scripts/question1.py
+++ scripts/question1.py
@@ -53,6 +53,7 @@ X_test = load('data/X_test_CC.npy')
 y_test = load('data/y_test_CC.npy')
 X_val = load('data/X_val_CC.npy')
 y_val = load('data/y_val_CC.npy')
+
 #%% Load numpy array from npy file - D
 X_train = load('data/X_train_D.npy')
 y_train = load('data/y_train_D.npy')
@@ -60,6 +61,7 @@ X_test = load('data/X_test_D.npy')
 y_test = load('data/y_test_D.npy')
 X_val = load('data/X_val_D.npy')
 y_val = load('data/y_val_D.npy')
+
 #%% Load numpy array from npy file - Y
 X_train = load('data/X_train_Y.npy')
 y_train = load('data/y_train_Y.npy')
