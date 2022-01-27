8547d7865dba8f0c88d9a9727bc6d5f6e7cb719c
ADernild <alex@dernild.dk>
2022-01-27 12:12:36 +0100

    Visualized class distributions

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -34,10 +34,13 @@ df['Y'] = np.select([(df[1].astype(str).str.len()>1) & (df[1].astype(str).str.le
 
 #%% Plotting class distributions
 sns.histplot(df['CC'])
+plt.savefig('CC_dist.png', dpi=300)
 plt.figure()
 sns.histplot(df['D'])
+plt.savefig('D_dist.png', dpi=300)
 plt.figure()
 sns.histplot(df['Y'])
+plt.savefig('Y_dist.png', dpi=300)
 
 #%% reassign 5,6,7,8,9 to class 10 in D
 df.loc[df.D > 4, 'D'] = 10
@@ -49,6 +52,7 @@ df = df.rename(columns={0: "Name", 1: "Label"}) # Add name to label column
 sns.histplot(df['CC'])
 plt.figure()
 sns.histplot(df['D'])
+plt.savefig('D_dist_new.png', dpi=300)
 plt.figure()
 sns.histplot(df['Y'])
 
@@ -147,7 +151,15 @@ X_test_Y, y_test_Y = splitfolder_to_array(Categories=['0','1','2','3','4','5','6
 X_val_Y, y_val_Y = splitfolder_to_array(Categories=['0','1','2','3','4','5','6','7','8','9','10'], datadir='data/split/Y/val')
 print(X_train_Y.shape, X_test_Y.shape, y_train_Y.shape, y_test_Y.shape, X_val_Y.shape, y_val_Y.shape)
 
-#%% Save numpy array as npy file
+#%% Save numpy array as npy file - Anders
+#l1 = ['X_train_', 'y_train_', 'X_test_', 'y_test_', 'X_val_', 'y_val_']
+#l2 = ['CC', 'D', 'Y']
+#for i in l1:
+#    for j in l2: 
+#        a = f"'data/{i}{j}.npy'"
+#        b = f'{i}{j}'
+#        save(a,b)
+
 #from numpy import asarray
 #CC
 save('data/X_train_CC.npy', X_train_CC)
