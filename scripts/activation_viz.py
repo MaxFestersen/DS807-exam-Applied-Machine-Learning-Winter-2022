3876aeffad958a4652404f49385ff902152a82b8
ADernild <alex@dernild.dk>
2022-01-30 10:19:29 +0100

    plotting activations first layer

---
+++ scripts/activation_viz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 29 23:49:03 2022
+
+@author: adernild
+"""
+import PIL
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from numpy import expand_dims
+
+
+model = load_model('models/modelD_aug')
+
+img = PIL.Image.open('data/DIDA_2/46.jpg')
+img = img_to_array(img)
+img = expand_dims(img, axis=0)
+
+plt.figure(figsize=(10, 10))
+for i in range(16):
+    ax = plt.subplot(4, 4, i + 1); 
+    plt.axis('off'); 
+    plt.imshow(model.get_layer('conv2d_50')(img)[0, :, :, i])
+    
